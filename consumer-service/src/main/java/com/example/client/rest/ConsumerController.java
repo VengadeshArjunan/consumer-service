2d3f4460edfbc7964639d349357481740c64c8bb
Admin <Admin@Admin-PC>
2023-03-26 14:29:42 +0530

    first commit

---
+++ consumer-service/src/main/java/com/example/client/rest/ConsumerController.java
@@ -0,0 +1,17 @@
+package com.example.client.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ConsumerController {
+	@Autowired
+	private RestTemplate restTemplate;
+	@GetMapping("/message")
+	public String message() {
+		String url = "http://producer-service/message";
+		return restTemplate.getForObject(url, String.class);
+	}
+}
