7693f9792e93598f7f6b160af495db10097024d5
Admin <Admin@Admin-PC>
2023-03-31 17:24:09 +0530

    second commit

--- consumer-service/src/main/java/com/example/client/rest/ConsumerController.java
+++ consumer-service/src/main/java/com/example/client/rest/ConsumerController.java
@@ -2,10 +2,12 @@ package com.example.client.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequestMapping("/consumer")
 public class ConsumerController {
 	@Autowired
 	private RestTemplate restTemplate;
