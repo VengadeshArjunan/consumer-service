fe7412c060c266e232fdf8379958017937ae8a33
Admin <Admin@Admin-PC>
2023-04-09 10:50:17 +0530

    second commit

--- consumer-service/src/main/java/com/example/client/rest/ConsumerController.java
+++ consumer-service/src/main/java/com/example/client/rest/ConsumerController.java
@@ -1,7 +1,10 @@
 package com.example.client.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -11,9 +14,24 @@ import org.springframework.web.client.RestTemplate;
 public class ConsumerController {
 	@Autowired
 	private RestTemplate restTemplate;
+	
+	Logger logger = LoggerFactory.getLogger(ConsumerController.class);
+	
 	@GetMapping("/message")
 	public String message() {
+		logger.debug("*****Started fetching data from consumer using endpoint /message");
 		String url = "http://producer-service/message";
-		return restTemplate.getForObject(url, String.class);
+		String respose =  restTemplate.getForObject(url, String.class);
+		logger.debug("*****Finished fetching data from consumer using endpoint /message");
+		return respose;
+	}
+	
+	@GetMapping("/message/{id}")
+	public String message(@PathVariable long id) {
+		logger.debug("*****Started data from consumer using endpoint /message/id");
+		String url = "http://producer-service/message"+id;
+		String respose = restTemplate.getForObject(url, String.class);
+		logger.debug("*****Finished fetching data from consumer using endpoint /message/id");
+		return respose;
 	}
 }
